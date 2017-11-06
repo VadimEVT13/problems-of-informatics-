b9736868dbed66865b77f6a0f2600761e9e55501
Agent056 <32037655+Agent056@users.noreply.github.com>
2017-11-07 00:20:50 +0500

    Метод сравнения добавлен

--- Sravnenie/Form1.cs
+++ Sravnenie/Form1.cs
@@ -24,11 +24,7 @@ namespace Sravnenie
         private void button1_Click(object sender, EventArgs e)
         {
             SimMetricsMetricUtilities.Levenstein ex_l = new SimMetricsMetricUtilities.Levenstein();
-
-            Jaro our_j = new Jaro();
-            JaroVincler our_jv = new JaroVincler();
-            Levenstein our_l = new Levenstein();
-
+            
             Stopwatch t = new Stopwatch();
             /*
                 t.Start();
@@ -39,15 +35,75 @@ namespace Sravnenie
             string s1 = "Vadim";
             string s2 = "Vidam";
 
-            double rezj1 = our_j.Func(s1, s2);
+            double rezj1 = Jaro.Func(s1, s2);
             double rezj3 = ExJaro.distance(s1, s2);
 
 
-            double rezjv1 = our_jv.Func(s1, s2);
+            double rezjv1 = JaroVincler.Func(s1, s2);
             double rezjv3 = ExJaroWincler.distance(s1, s2);
 
-            double rezl1 = our_l.Func(s1, s2);
+            double rezl1 = Levenstein.Func(s1, s2);
             double rezl2 = ex_l.GetSimilarity(s1, s2);
         }
+
+        private List<string[]> Sravn(string[][] data)
+        {
+            List<string[]> rezlist = new List<string[]>();
+
+            foreach (string[] str in data)
+            {
+                // количество слов в строке
+                if (str.Count() != 2)
+                    continue;
+                else
+                {
+                    string[] mass;
+
+                    SimMetricsMetricUtilities.Levenstein ex_l = new SimMetricsMetricUtilities.Levenstein();\
+
+                    Stopwatch t = new Stopwatch();
+                    t.Start();
+                    double rj1 = Jaro.Func(str[0], str[1]);
+                    string tj1 = t.Elapsed.ToString();
+
+                    t = new Stopwatch();
+                    t.Start();                                  
+                    double rj2 = ExJaro.distance(str[0], str[1]);
+                    string tj2 = t.Elapsed.ToString();
+                                
+                    // -----
+
+                    t = new Stopwatch();
+                    t.Start();
+                    double rjv1 = JaroVincler.Func(str[0], str[1]);
+                    string tjv1 = t.Elapsed.ToString();
+
+                    t = new Stopwatch();
+                    t.Start();
+                    double rjv2 = ExJaroWincler.distance(str[0], str[1]);
+                    string tjv2 = t.Elapsed.ToString();
+                                        
+                    // ----
+
+                    t = new Stopwatch();
+                    t.Start();
+                    double rl1 = Levenstein.Func(str[0], str[1]);
+                    string tl1 = t.Elapsed.ToString();
+
+                    t = new Stopwatch();
+                    t.Start();
+                    double rl2 = ex_l.GetSimilarity(str[0], str[1]);
+                    string tl2 = t.Elapsed.ToString();
+                                        
+                    rezlist.Add(new string[12] { rj1.ToString(), tj1, rj2.ToString(), tj2,
+                        rjv1.ToString(), tjv1, rjv2.ToString(), tjv2,
+                        rl1.ToString(), tl1, rl2.ToString(), tl2});
+                }
+            }
+
+            return rezlist;
+        }
+
+
     }
 }
